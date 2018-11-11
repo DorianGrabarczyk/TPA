70095d315e1a7983a4c19b9d7d242342d6f8a9d8
DorianGrabarczyk <43935979+DorianGrabarczyk@users.noreply.github.com>
2018-11-11 22:20:52 +0100

    Adding model

---
+++ TPA_Etap_1/ViewModel/Reflector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TPA.Reflection.Model;
+
+namespace ViewModel
+{
+    public class Reflector
+    {
+        public AssemblyMetadata AssemblyModel { get; set; }
+
+        public void Reflect(string assemblyFile)
+        {
+            Assembly assembly = Assembly.LoadFrom(assemblyFile);
+            AssemblyModel = new AssemblyMetadata(assembly);
+        }
+
+        public void Reflect(Assembly assembly)
+        {
+            AssemblyModel = new AssemblyMetadata(assembly);
+        }
+    }
+}
