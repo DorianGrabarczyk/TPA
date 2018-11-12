8e4a958de021cdf51cb71845e46e238e30ddcf0c
Dominik Ciesielski <dominik.ciesielski1997@gmail.pl>
2018-11-12 15:55:18 +0100

    Deleted the .vs folder from the repostiory.

--- TPA_Etap_1/ViewModel/ViewItems/PropertiesMetadataView.cs
+++ TPA_Etap_1/ViewModel/ViewItems/PropertiesMetadataView.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TPA_Etap_1.Reflection.Model;
 
 namespace ViewModel.ViewItems
 {
-    class PropertiesMetadataView
+    class PropertiesMetadataView : ITree
     {
+        private IEnumerable<PropertyMetadata> _properties;
+
+        public PropertiesMetadataView(IEnumerable<PropertyMetadata> properties)
+        {
+            _properties = properties;
+        }
+
+        public override string Name => this.ToString();
+        public override bool Expandable => _properties?.Count() > 0;
+
+        public override void LoadChildren()
+        {
+            //...
+        }
+
+        public override string ToString()
+        {
+            return "Properties:";
+        }
     }
 }
