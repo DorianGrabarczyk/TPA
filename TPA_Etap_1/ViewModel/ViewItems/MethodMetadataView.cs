4e774b3b7d44a22766dd99d121e3fe8e3627a81d
Dominik Ciesielski <dominik.ciesielski1997@gmail.pl>
2018-11-12 16:19:54 +0100

    New classes created in ViewModel.

---
+++ TPA_Etap_1/ViewModel/ViewItems/MethodMetadataView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPA_Etap_1.Reflection.Model;
+
+namespace ViewModel.ViewItems
+{
+    class MethodMetadataView : ITree
+    {
+        private MethodMetadata _method;
+
+        public override string Name => this.ToString();
+
+        public override void LoadChildren()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
