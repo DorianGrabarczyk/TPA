6eb340e258282cb8cae4f8d316bdd5e73c885a3c
Dominik Ciesielski <dominik.ciesielski1997@gmail.pl>
2018-11-12 15:18:56 +0100

    Small changes to the project - added two additional classes to ViewModel. I also used the chance to clean up the repository and modify the .gitignore file.

---
+++ TPA_Etap_1/ViewModel/ViewItems/PropertyMetaDataView.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPA_Etap_1.Reflection.Model;
+
+namespace ViewModel.ViewItems
+{
+    class PropertyMetadataView : ITree
+    {
+        private PropertyMetadata _property;
+
+        public PropertyMetadataView(PropertyMetadata property)
+        {
+            _property = property;
+        }
+
+        public override string Name => this.ToString();
+        public override bool Expandable => _property.m_Name.Count() > 0;
+
+        public override void LoadChildren()
+        {
+            //Children.Clear();
+        }
+
+        public override string ToString()
+        {
+            return "Property: " + _property.m_Name;
+        }
+    }
+}
