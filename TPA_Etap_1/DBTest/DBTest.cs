659d908fe57c307e2fa3e9fc8f6179bd5c7fabff
redhairone <dominik.ciesielski1997@gmail.com>
2019-01-30 19:22:43 +0100

    Test Fixes

--- TPA_Etap_1/DBTest/DBTest.cs
+++ TPA_Etap_1/DBTest/DBTest.cs
@@ -21,19 +21,9 @@ namespace DBTest
             Reflector reflector = new Reflector();
             reflector.Reflect(pathh);
             ISerializer ser = new DatabaseSerializer();
-            string path = Directory.GetParent(Directory.GetCurrentDirectory()).ToString();
-            string root = "DBTest";
-            while (!(path.Substring(path.Length - root.Length) == root))
-            {
-                Console.WriteLine(path);
-                path = path.Remove(path.Length - 1);
-            }
-            path += "\\Database2.mdf";
-            string a = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = ";
-            string b = @"; Integrated Security = True; Connect Timeout = 30";
-            DBModelContext db = new DBModelContext(a + path + b);
-            ser.Serialize(reflector.AssemblyModel.MapUp(),a+path+b);
-            AssemblyMetadata assemblyTest = new AssemblyMetadata(ser.Deserialize(a+path+b));
+            DBModelContext db = new DBModelContext();
+            ser.Serialize(reflector.AssemblyModel.MapUp(), null);
+            AssemblyMetadata assemblyTest = new AssemblyMetadata(ser.Deserialize(null));
 
 
             Assert.AreEqual(4, assemblyTest.m_Namespaces.ToList().Count());
@@ -43,19 +33,9 @@ namespace DBTest
             Reflector reflector = new Reflector();
             reflector.Reflect(pathh);
             ISerializer ser = new DatabaseSerializer();
-            string path = Directory.GetParent(Directory.GetCurrentDirectory()).ToString();
-            string root = "DBTest";
-            while (!(path.Substring(path.Length - root.Length) == root))
-            {
-                Console.WriteLine(path);
-                path = path.Remove(path.Length - 1);
-            }
-            path += "\\Database2.mdf";
-            string a = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = ";
-            string b = @"; Integrated Security = True; Connect Timeout = 30";
-            DBModelContext db = new DBModelContext(a + path + b);
-            ser.Serialize(reflector.AssemblyModel.MapUp(), a + path + b);
-            AssemblyMetadata assemblyTest = new AssemblyMetadata(ser.Deserialize(a + path + b));
+            DBModelContext db = new DBModelContext();
+            ser.Serialize(reflector.AssemblyModel.MapUp(), null);
+            AssemblyMetadata assemblyTest = new AssemblyMetadata(ser.Deserialize(null));
 
             List<TypeMetadata> testLibraryTypes = assemblyTest.m_Namespaces.ToList().Find(t => t.m_NamespaceName == "TPA.ApplicationArchitecture.BusinessLogic").m_Types.ToList();
 
@@ -68,19 +48,9 @@ namespace DBTest
             Reflector reflector = new Reflector();
             reflector.Reflect(pathh);
             ISerializer ser = new DatabaseSerializer();
-            string path = Directory.GetParent(Directory.GetCurrentDirectory()).ToString();
-            string root = "DBTest";
-            while (!(path.Substring(path.Length - root.Length) == root))
-            {
-                Console.WriteLine(path);
-                path = path.Remove(path.Length - 1);
-            }
-            path += "\\Database2.mdf";
-            string a = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = ";
-            string b = @"; Integrated Security = True; Connect Timeout = 30";
-            DBModelContext db = new DBModelContext(a + path + b);
-            ser.Serialize(reflector.AssemblyModel.MapUp(), a + path + b);
-            AssemblyMetadata assemblyTest = new AssemblyMetadata(ser.Deserialize(a + path + b));
+            DBModelContext db = new DBModelContext();
+            ser.Serialize(reflector.AssemblyModel.MapUp(), null);
+            AssemblyMetadata assemblyTest = new AssemblyMetadata(ser.Deserialize(null));
 
             List<TypeMetadata> test = assemblyTest.m_Namespaces.ToList().Find(t => t.m_NamespaceName == "TPA.ApplicationArchitecture.BusinessLogic").m_Types.ToList();
             Assert.AreEqual(1, test.First().m_Properties.ToList().Count);
@@ -91,19 +61,9 @@ namespace DBTest
             Reflector reflector = new Reflector();
             reflector.Reflect(pathh);
             ISerializer ser = new DatabaseSerializer();
-            string path = Directory.GetParent(Directory.GetCurrentDirectory()).ToString();
-            string root = "DBTest";
-            while (!(path.Substring(path.Length - root.Length) == root))
-            {
-                Console.WriteLine(path);
-                path = path.Remove(path.Length - 1);
-            }
-            path += "\\Database2.mdf";
-            string a = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = ";
-            string b = @"; Integrated Security = True; Connect Timeout = 30";
-            DBModelContext db = new DBModelContext(a + path + b);
-            ser.Serialize(reflector.AssemblyModel.MapUp(), a + path + b);
-            AssemblyMetadata assemblyTest = new AssemblyMetadata(ser.Deserialize(a + path + b));
+            DBModelContext db = new DBModelContext();
+            ser.Serialize(reflector.AssemblyModel.MapUp(), null);
+            AssemblyMetadata assemblyTest = new AssemblyMetadata(ser.Deserialize(null));
 
             List<TypeMetadata> test = assemblyTest.m_Namespaces.ToList().Find(t => t.m_NamespaceName == "TPA.ApplicationArchitecture.BusinessLogic").m_Types.ToList();
             Assert.AreEqual(6, test.First().m_Methods.ToList().Count);
